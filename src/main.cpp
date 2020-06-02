a6bf490f3b719620bcf7aea215915b28d8f7e39c
Fabio Rossini Sluzala <fabio3rs@gmail.com>
2020-06-02 15:06:56 -0300

    Permutating

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,92 @@
 #include <iostream>
+#include <array>
+#include <vector>
+#include <cstdint>
+#include <algorithm>
 #include "crc32.h"
+
+void findcollisions(uint32_t hash, int length, std::string perm_list)
+{
+    if (perm_list.size() == 0)
+        return;
+
+    std::array<uint32_t, 64> hashbylen;
+    char str[128] = { 0 };
+
+    std::sort(perm_list.begin(), perm_list.end());
+    std::cout << "Permutation list " << perm_list << std::endl;
+    int ppos = 0;
+    int end = perm_list[perm_list.size() - 1];
+
+    for (int i = 1; i < length; i++)
+    {
+        for (int j = 0; j < i; j++)
+            str[j] = perm_list[0];
+
+        while (true)
+        {
+            for (int j = 0; j < perm_list.size(); j++)
+            {
+                str[i] = perm_list[j];
+                std::cout << str << std::endl;
+            }
+            
+            bool next = false;
+
+            for (int l = i - 1; l >= 0; l--)
+            {
+                auto it = std::find(perm_list.begin(), perm_list.end(), str[l]);
+
+                if (it == perm_list.end())
+                {
+                    std::cout << "error";
+                }
+
+                ++it;
+                if (it != perm_list.end())
+                {
+                    str[l] = *it;
+
+                    for (int j = l + 1; j < i; j++)
+                        str[j] = perm_list[0];
+                    
+                    next = true;
+                    break;
+                }
+            }
+            
+            if (!next)
+                break;
+        }
+    }
+
+    /*for (int i = 1; i < length; i++)
+    {
+        for (int j = 0; j < i; j++)
+            str[j] = perm_list[0];
+
+        for (int l = i - 1; l >= 0; l--)
+        {
+            for (int p = 1; p < perm_list.size(); p++)
+            {
+                std::cout << "l " << l << std::endl;
+                for (int j = 0; j < perm_list.size(); j++)
+                {
+                    str[i] = perm_list[j];
+                    std::cout << str << std::endl;
+                }
+
+                str[l] = perm_list[p];
+            }
+        }
+    }*/
+}
 
 int main(int argc, char *argv[])
 {
+    findcollisions(0, 4, "ABCDEFGHIJKLMNOPQRTUVWXYZ");
     
+
     return 0;    
 }
 
