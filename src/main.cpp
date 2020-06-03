ca310bd442cc1e8a5b1613a4918e75d7c3231047
Fabio Rossini Sluzala <fabio3rs@gmail.com>
2020-06-02 23:12:05 -0300

    Some IO optimizations

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,41 @@
 #include <memory>
 #include <chrono>
 #include <thread>
+#include <atomic>
+#include <cstring>
 #include "crc32.h"
+
+std::mutex printmutex;
+
+struct collision_data
+{
+    uint32_t hash;
+    uintptr_t thread_id;
+    std::chrono::time_point<std::chrono::high_resolution_clock> when;
+    char str[128];
+
+    collision_data()
+    {
+        hash = 0;
+        thread_id = 0;
+        str[0] = 0;
+    }
+};
+
+std::string io_buffer;
+std::vector<collision_data> io_collisions;
+
+void register_collision(uint32_t hash, std::chrono::time_point<std::chrono::high_resolution_clock> when, uintptr_t id, const char *str, int strsize)
+{
+    std::lock_guard<std::mutex> lck(printmutex);
+    io_collisions.push_back({});
+    
+    collision_data &col = io_collisions.back();
+    col.hash = hash;
+    col.when = when;
+    col.thread_id = id;
+    strncpy(col.str, str, strsize > sizeof(col.str)? (sizeof(col.str) - 1) : strsize);
+}
 
 struct permdata
 {
@@ -27,8 +61,10 @@ struct permdata
 
 permdata lastpdata;
 std::mutex assignpermmutx;
-std::mutex printmutex;
 
+/*
+Each thread process all the permutations with certain length starting with some letter
+*/
 permdata assignthreadnewperm(int len, int perm, const std::string &perm_list)
 {
     std::lock_guard<std::mutex> lck(assignpermmutx);
@@ -43,7 +79,7 @@ permdata assignthreadnewperm(int len, int perm, const std::string &perm_list)
     return lastpdata;
 }
 
-void findcollisions_mthread(uint32_t hash, int length, std::string perm_list, std::chrono::time_point<std::chrono::high_resolution_clock> start)
+void findcollisions_mthread(uint32_t hash, int length, std::string perm_list, uintptr_t thread_id)
 {
     if (perm_list.size() == 0)
         return;
@@ -52,15 +88,10 @@ void findcollisions_mthread(uint32_t hash, int length, std::string perm_list, st
     char str[128] = { 0 };
 
     std::sort(perm_list.begin(), perm_list.end());
-    int ppos = 0;
 
     permdata pd = assignthreadnewperm(0, 0, perm_list);
-    /*{
-        std::lock_guard<std::mutex> lck(printmutex);
-        std::cout << pd.len << "  " << pd.perm << std::endl;
-    }*/
 
-    for (int i = pd.len; i < length; i++)
+    for (int i = pd.len; i < length; /*i++*/)
     {
         i = pd.len;
 
@@ -75,16 +106,16 @@ void findcollisions_mthread(uint32_t hash, int length, std::string perm_list, st
             
             for (int j = 0; j < perm_list.size(); j++)
             {
-                str[i] = perm_list[j];
-                uint32_t resulthash = updateCrc32String(hashbase, &str[i], 1);
+                uint32_t resulthash = updateCrc32String(hashbase, &perm_list[j], 1);
 
                 if (resulthash == hash)
                 {
-                    auto nowtime = std::chrono::high_resolution_clock::now();
-                    std::chrono::duration<double> diff = nowtime-start;
+                    // complete the string
+                    str[i] = perm_list[j];
 
-                    std::lock_guard<std::mutex> lck(printmutex);
-                    std::cout << diff.count() << "      " << str << "\n";
+                    // Send to IO
+                    auto nowtime = std::chrono::high_resolution_clock::now();
+                    register_collision(hash, nowtime, thread_id, str, i + 1);
                 }
             }
             
@@ -185,19 +216,73 @@ void findcollisions(uint32_t hash, int length, std::string perm_list)
     }
 }
 
+std::atomic<bool> iothreadShouldContinue;
+
 /**/
+void io_thread(std::chrono::time_point<std::chrono::high_resolution_clock> start)
+{
+    std::string buffer;
+    std::string temp;
+    temp.reserve(64);
+    buffer.reserve(2048);
+
+    while (iothreadShouldContinue)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        
+        {
+            std::lock_guard<std::mutex> lck(printmutex);
+
+            if (io_collisions.size() > 0)
+            {
+                buffer.clear();
+
+                for (collision_data &io : io_collisions)
+                {
+                    temp.clear();
+                    std::chrono::duration<double> diff = io.when - start;
+
+                    temp += std::to_string(diff.count());
+                    temp.append(12 - temp.size(), ' ');
+                    temp += "  #";
+                    temp += std::to_string(io.thread_id);
+                    temp += "  ";
+                    buffer += temp;
+
+                    buffer.append(32 - temp.size(), ' ');
+                    buffer += io.str;
+                    buffer += "\n";
+                }
+
+                io_collisions.clear();
+            }
+        }
+
+        if (buffer.size() > 0)
+        {
+            std::cout << buffer;
+            buffer.clear();
+        }
+    }
+}
 
 int main(int argc, char *argv[])
 {
-    auto starttime = std::chrono::high_resolution_clock::now();
+    iothreadShouldContinue = true;
+
+    io_collisions.reserve(16);
+
     int threads = std::thread::hardware_concurrency();
     std::vector<std::thread> thrds;
     thrds.reserve(threads);
 
-    std::cout << "TIME          STRING" << std::endl;
+    auto starttime = std::chrono::high_resolution_clock::now();
+    std::thread iothred(io_thread, starttime);
+
+    std::cout << "TIME          THREAD            STRING" << std::endl;
     for (int i = 0; i < threads; i++)
     {
-        thrds.push_back(std::thread(findcollisions_mthread, 0xDE4B237D, 16, "ABCDEFGHIJKLMNOPQRTUVWXYZ", starttime));
+        thrds.push_back(std::thread(findcollisions_mthread, 0xDE4B237D, 16, "ABCDEFGHIJKLMNOPQRTUVWXYZ", i + 1));
     }
 
     //findcollisions(0xDE4B237D, 16, "ABCDEFGHIJKLMNOPQRTUVWXYZ");
@@ -207,6 +292,12 @@ int main(int argc, char *argv[])
         if (t.joinable())
             t.join();
     }
-    return 0;    
+
+    iothreadShouldContinue = false;
+
+    if (iothred.joinable())
+        iothred.join();
+    
+    return 0;
 }
 
