ef70933a76826a40702302f76db0980fea77014d
Fabio Rossini Sluzala <fabio3rs@gmail.com>
2020-06-15 01:26:34 -0300

    More constexpr on optliststruct

--- src/main.cpp
+++ src/main.cpp
@@ -147,7 +147,7 @@ permdata assignthreadnewperm(int len, int perm, const std::string &perm_list)
     return lastpdata;
 }
 
-
+// https://stackoverflow.com/questions/19559808/constexpr-initialization-of-array-to-sort-contents
 template<class T, std::size_t N>
 struct c_array
 {
@@ -173,8 +173,31 @@ struct c_array
 template<class T>
 struct c_array<T, 0> {};
 
+
+struct optliststruct
+{
+    int pos;
+    int end;
+
+    constexpr optliststruct() : pos(-1), end(0)
+    {
+
+    }
+};
+
 constexpr c_array<uint32_t, 87> cheatArray()
 {
+    /*
+    0xDE4B237D, 0xB22A28D1, 0x5A783FAE, 0xEECCEA2B, 0x42AF1E28, 0x555FC201, 0x2A845345, 0xE1EF01EA, 0x771B83FC, 0x5BF12848,
+        0x44453A17, 0xFCFF1D08, 0xB69E8532, 0x8B828076, 0xDD6ED9E9, 0xA290FD8C, 0x3484B5A7, 0x43DB914E, 0xDBC0DD65, 0x00000000, 0xD08A30FE, 0x37BF1B4E,
+        0xB5D40866, 0xE63B0D99, 0x675B8945, 0x4987D5EE, 0x2E8F84E8, 0x1A9AA3D6, 0xE842F3BC, 0x0D5C6A4E, 0x74D4FCB1, 0xB01D13B8, 0x66516EBC, 0x4B137E45,
+        0x00000000, 0x78520E33, 0x3A577325, 0xD4966D59, 0x5FD1B49D, 0xA7613F99, 0x1792D871, 0xCBC579DF, 0x4FEDCCFF, 0x44B34866, 0x2EF877DB, 0x2781E797,
+        0x2BC1A045, 0xB2AFE368, 0xFA8DD45B, 0x8DED75BD, 0x1A5526BC, 0xA48A770B, 0xB07D3B32, 0x80C1E54B, 0x5DAD0087, 0x7F80B950, 0x6C0FA650, 0xF46F2FA4,
+        0x70164385, 0x00000000, 0x885D0B50, 0x151BDCB3, 0xADFA640A, 0xE57F96CE, 0x040CF761, 0xE1B33EB9, 0xFEDA77F7, 0x8CA870DD, 0x9A629401, 0xF53EF5A5,
+        0xF2AA0C1D, 0xF36345A8, 0x8990D5E1, 0xB7013B1B, 0xCAEC94EE, 0x31F0C3CC, 0xB3B3E72A, 0xC25CDBFF, 0xD5CF4EFF, 0x680416B1, 0xCF5FDA18, 0xF01286E9,
+        0xA841CC0A, 0x31EA09CF, 0xE958788A, 0x02C83A7C, 0xE49C3ED4, 0x171BA8CC, 0x86988DAE, 0x2BDD2FA1, 0x00000000, 0x00000000
+    */
+   
     c_array<uint32_t, 87> result{{ 0xDE4B237D, 0xB22A28D1, 0x5A783FAE, 0xEECCEA2B, 0x42AF1E28, 0x555FC201, 0x2A845345, 0xE1EF01EA, 0x771B83FC, 0x5BF12848,
         0x44453A17, 0xFCFF1D08, 0xB69E8532, 0x8B828076, 0xDD6ED9E9, 0xA290FD8C, 0x3484B5A7, 0x43DB914E, 0xDBC0DD65, 0xD08A30FE, 0x37BF1B4E,
         0xB5D40866, 0xE63B0D99, 0x675B8945, 0x4987D5EE, 0x2E8F84E8, 0x1A9AA3D6, 0xE842F3BC, 0x0D5C6A4E, 0x74D4FCB1, 0xB01D13B8, 0x66516EBC, 0x4B137E45,
@@ -202,6 +225,29 @@ constexpr c_array<uint32_t, 87> cheatArray()
     return result;
 }
 
+template<uint32_t OPTSIZE, uint32_t START, uint32_t DIVISOR>
+constexpr c_array<optliststruct, OPTSIZE> gentblvec(const c_array<uint32_t, 87> &ct)
+{
+    c_array<optliststruct, OPTSIZE> tblvec;
+
+    for (int i = 0, size = ct.size(); i < size; i++)
+    {
+        uint32_t BASE = ct[i] - START;
+
+        auto &op = tblvec[BASE / DIVISOR];
+
+        if (op.pos == -1)
+        {
+            op.pos = i;
+            op.end = i;
+        }
+            
+        op.end++;
+    }
+
+    return tblvec;
+}
+
 void findcollisions_mthread(uint32_t hash, int length, const std::string &perm_list, uintptr_t thread_id)
 {
     if (perm_list.size() == 0)
@@ -210,16 +256,6 @@ void findcollisions_mthread(uint32_t hash, int length, const std::string &perm_l
     if (length > 31)
         length = 31;
 
-    /*
-    0xDE4B237D, 0xB22A28D1, 0x5A783FAE, 0xEECCEA2B, 0x42AF1E28, 0x555FC201, 0x2A845345, 0xE1EF01EA, 0x771B83FC, 0x5BF12848,
-        0x44453A17, 0xFCFF1D08, 0xB69E8532, 0x8B828076, 0xDD6ED9E9, 0xA290FD8C, 0x3484B5A7, 0x43DB914E, 0xDBC0DD65, 0x00000000, 0xD08A30FE, 0x37BF1B4E,
-        0xB5D40866, 0xE63B0D99, 0x675B8945, 0x4987D5EE, 0x2E8F84E8, 0x1A9AA3D6, 0xE842F3BC, 0x0D5C6A4E, 0x74D4FCB1, 0xB01D13B8, 0x66516EBC, 0x4B137E45,
-        0x00000000, 0x78520E33, 0x3A577325, 0xD4966D59, 0x5FD1B49D, 0xA7613F99, 0x1792D871, 0xCBC579DF, 0x4FEDCCFF, 0x44B34866, 0x2EF877DB, 0x2781E797,
-        0x2BC1A045, 0xB2AFE368, 0xFA8DD45B, 0x8DED75BD, 0x1A5526BC, 0xA48A770B, 0xB07D3B32, 0x80C1E54B, 0x5DAD0087, 0x7F80B950, 0x6C0FA650, 0xF46F2FA4,
-        0x70164385, 0x00000000, 0x885D0B50, 0x151BDCB3, 0xADFA640A, 0xE57F96CE, 0x040CF761, 0xE1B33EB9, 0xFEDA77F7, 0x8CA870DD, 0x9A629401, 0xF53EF5A5,
-        0xF2AA0C1D, 0xF36345A8, 0x8990D5E1, 0xB7013B1B, 0xCAEC94EE, 0x31F0C3CC, 0xB3B3E72A, 0xC25CDBFF, 0xD5CF4EFF, 0x680416B1, 0xCF5FDA18, 0xF01286E9,
-        0xA841CC0A, 0x31EA09CF, 0xE958788A, 0x02C83A7C, 0xE49C3ED4, 0x171BA8CC, 0x86988DAE, 0x2BDD2FA1, 0x00000000, 0x00000000
-    */
     constexpr c_array<uint32_t, 87> cheatTable = cheatArray();
 
     const uint32_t LAST = cheatTable[cheatTable.size() - 1];
@@ -227,43 +263,7 @@ void findcollisions_mthread(uint32_t hash, int length, const std::string &perm_l
     const uint32_t START = cheatTable[0], DIVISOR = (DIFF / cheatTable.size());
     const uint32_t OPTSIZE = LAST / DIVISOR + 1;
 
-    struct optliststruct
-    {
-        int pos;
-        int end;
-
-        optliststruct()
-        {
-            pos = -1;
-            end = 0;
-        }
-    };
-
-    std::vector<optliststruct> tblvec;
-
-    tblvec.reserve(OPTSIZE);
-
-    {
-        for (int i = 0; i < OPTSIZE; i++)
-        {
-            tblvec.push_back({});
-        }
-        
-        for (int i = 0, size = cheatTable.size(); i < size; i++)
-        {
-            uint32_t BASE = cheatTable[i] - START;
-
-            auto &op = tblvec[BASE / DIVISOR];
-
-            if (op.pos == -1)
-            {
-                op.pos = i;
-                op.end = i;
-            }
-            
-            op.end++;
-        }
-    }
+    constexpr c_array<optliststruct, OPTSIZE> tblvec = gentblvec<OPTSIZE, START, DIVISOR>(cheatTable);
 
     std::array<uint32_t, 32> hashbylen;
     char str[32] = { 0 };
